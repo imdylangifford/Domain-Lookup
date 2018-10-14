141e88e6a3568ca3dd200e154758fb0a9cde8a93
Dylan <imdylangifford@gmail.com>
2018-10-14 17:10:48 -0600

    added deployment script

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -5,7 +5,7 @@ import "./Home.css";
 export default function Home() {
   return (
     <div className="Home">
-      <h1>WHOIS LOOKUP</h1>
+      <h1>DOMAIN LOOKUP</h1>
       <h2>GET INFORMATION ON ANY DOMAIN</h2>
       <Link to="/main">
         <p className="getstarted">GET STARTED</p>
