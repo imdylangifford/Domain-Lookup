48bdff8a150c58e8227f5dd8fff9ee8c75d6b45a
Dylan <imdylangifford@gmail.com>
2018-10-15 15:20:25 -0600

    fixed issue where particle bogged down on mobile screens

--- src/App.js
+++ src/App.js
@@ -13,56 +13,11 @@ class App extends Component {
           className="canvas"
           params={{
             particles: {
-              number: {
-                value: 80,
-                density: {
-                  enable: true,
-                  value_area: 800
-                }
-              },
-              color: {
-                value: "#000"
-              },
-              shape: {
-                type: "square",
-                stroke: {
-                  width: 5,
-                  color: "#000000"
-                },
-                polygon: {
-                  nb_sides: 8
-                }
-              },
-              opacity: {
-                value: 0.5,
-                random: false,
-                anim: {
-                  enable: false,
-                  speed: 10,
-                  opacity_min: 0.1,
-                  sync: false
-                }
-              },
-
               line_linked: {
-                enable: true,
-                distance: 200,
-                color: "#000",
-                opacity: 0.6,
-                width: 1
-              },
-              move: {
-                enable: true,
-                speed: 6,
-                direction: "none",
-                random: true,
-                straight: false,
-                out_mode: "out",
-                bounce: false,
-                attract: {
-                  enable: false,
-                  rotateX: 600,
-                  rotateY: 1200
+                shadow: {
+                  enable: true,
+                  color: "#000000",
+                  blur: 1
                 }
               }
             }
